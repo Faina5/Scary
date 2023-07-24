9c2d3327b87c0ca91c5e0ca9038aa93da61f2ab1
Polina Ivanova <124639772+Faina5@users.noreply.github.com>
2023-07-24 06:21:14 +0300

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,24 @@
-function readMore(){
-  var dots = document.getElementById('dots');
-  var more = document.getElementById('more');
-  var btn = document.getElementById('btn');
+const menub = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
 
-  if(dots.style.display === 'none'){
-    dots.style.display='inline';
-    btn.innerHTML='Читать далее';
-    more.style.display='none';
+menub?.addEventListener('click', () => {
+    menu.classList.toggle('menu__list-item--active');
+  });
+
+  function readMore(){
+    var dots = document.getElementById('dots');
+    var more = document.getElementById('more');
+    var btn = document.getElementById('btn');
+  
+    if(dots.style.display === 'none'){
+      dots.style.display='inline';
+      btn.innerHTML='Читать далее';
+      more.style.display='none';
+    }
+  
+    else{
+      dots.style.display='none';
+      btn.innerHTML='Скрыть';
+      more.style.display='inline';
+    }
   }
-
-  else{
-    dots.style.display='none';
-    btn.innerHTML='Скрыть';
-    more.style.display='inline';
-  }
-}
-
